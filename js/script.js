d1810ccf9a2ca53755922f931bafc85558bb5f20
Batuhan Arican <batuhanarican@hotmail.com>
2019-05-14 11:27:18 +0200

    Fixed theme

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ var getmain = document.querySelector("main");
 var getbook = document.querySelector(".book");
 var getbook2 = document.querySelector(".book2");
 var gettheme = document.querySelector(".theme");
+var getcijfer = document.querySelectorAll(".cijfer .a, .cijfer .b");
 var getschets = document.querySelectorAll(".schets .a, .schets .b");
+var getnotities = document.querySelectorAll(".notities .a, .notities .b");
 
 var white = "ffffff";
 var offwhite = "e3dccf";
@@ -47,24 +49,47 @@ function changeColor() {
         for(var i=0; i < getschets.length; i++) {
             getschets[i].style.stroke = offwhite;
         }
+        for(var j=0; j < getcijfer.length; j++) {
+            getcijfer[j].style.fill = offwhite;
+        }
+        for(var k=0; k < getnotities.length; k++) {
+            getnotities[k].style.stroke = offwhite;
+        }
     }
     else if (document.getElementById("blue").checked) {
         getmain.style.backgroundColor = blue;
         for(var i=0; i < getschets.length; i++) {
             getschets[i].style.stroke = orange;
         }
-        
+        for(var j=0; j < getcijfer.length; j++) {
+            getcijfer[j].style.fill = orange;
+        }
+        for(var k=0; k < getnotities.length; k++) {
+            getnotities[k].style.stroke = orange;
+        }
     }
     else if (document.getElementById("orange").checked) {
         getmain.style.backgroundColor = orange;
         for(var i=0; i < getschets.length; i++) {
             getschets[i].style.stroke = offwhite;
         }
+        for(var j=0; j < getcijfer.length; j++) {
+            getcijfer[j].style.fill = offwhite;
+        }
+        for(var k=0; k < getnotities.length; k++) {
+            getnotities[k].style.stroke = offwhite;
+        }
     }
     else if (document.getElementById("blueprint").checked) {
         getmain.style.backgroundColor = blueprint;
         for(var i=0; i < getschets.length; i++) {
             getschets[i].style.stroke = white;
+        }
+        for(var j=0; j < getcijfer.length; j++) {
+            getcijfer[j].style.fill = white;
+        }
+        for(var k=0; k < getnotities.length; k++) {
+            getnotities[k].style.stroke = white;
         }
     }
 }

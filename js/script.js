c6d362c773f586f3f6d18db45734b506007d3a15
Batuhan Arican <batuhanarican@hotmail.com>
2019-05-20 12:42:50 +0200

    New card animation and better lighting (3D)

--- js/script.js
+++ js/script.js
@@ -1,104 +1,147 @@
-var getbutton = document.querySelector("button.perspective");
-var getmain = document.querySelector("main");
-var getbook = document.querySelector(".book");
-var getbook2 = document.querySelector(".book2");
-var gettheme = document.querySelector(".theme");
-var getcijfer = document.querySelectorAll(".cijfer .a, .cijfer .b");
-var getschets = document.querySelectorAll(".schets .a, .schets .b");
-var getnotities = document.querySelectorAll(".notities .a, .notities .b");
+var getButton = document.querySelector("button.perspective");
+var getMain = document.querySelector("main");
+var getGrid = document.querySelector(".grid");
+var getBovenkant = document.querySelector(".bovenkant");
+var getBinnenkant = document.querySelector(".binnenkant");
+var getBinnenkant2 = document.querySelector(".binnenkant-2");
+var getOnderkant = document.querySelector(".onderkant");
+var getRug = document.querySelector(".rug");
+var getTheme = document.querySelector(".theme");
+var getCijfer = document.querySelectorAll(".cijfer .a, .cijfer .b");
+var getSchets = document.querySelectorAll(".schets .a, .schets .b");
+var getNotities = document.querySelectorAll(".notities .a, .notities .b");
+var getContainer = document.querySelector(".checkmark");
+var getCard = document.querySelectorAll(".card");
+var getCardInput = document.querySelectorAll(".grid .container input");
+var getCardSchets = document.querySelectorAll(".card .schets");
 
 var white = "ffffff";
 var offwhite = "e3dccf";
 var red = "#d62019";
 var blue = "#7bccdf";
 var orange = "#ea5103";
-var blueprint = "#1445ad";
+var blueprint = "#0e3587";
+
+var themeColor = red;
+var strokeColor = offwhite;
 
 function perspective() {
-    getbutton.classList.toggle("active");
-    getmain.classList.toggle("main-animated");
-    getbook.classList.toggle("book-animated");
-    getbook2.classList.toggle("book2-animated");
-    gettheme.classList.toggle("theme-animated");
+    getButton.classList.toggle("active");
+    getMain.classList.toggle("main-animated");
+    getBovenkant.classList.toggle("bovenkant-animated");
+    getBinnenkant.classList.toggle("binnenkant-animated");
+    getBinnenkant2.classList.toggle("binnenkant-2-animated");
+    getOnderkant.classList.toggle("onderkant-animated");
+    getRug.classList.toggle("rug-animated");
+    getTheme.classList.toggle("theme-animated");
+    for (var i=0; i < getCard.length; i++) {
+        getCard[i].classList.toggle("card-animated");
+    }
+    
 }
 
 function checkButton() {
-    if (getbutton.classList.contains("active")) {
-    getbutton.innerHTML = "2D";
+    if (getButton.classList.contains("active")) {
+    getButton.innerHTML = "2D";
     }
     else {
-        getbutton.innerHTML = "3D";
+        getButton.innerHTML = "3D";
     }
 }
 
 function changeColor() {
-    
-//    var color = document.getElementsByName('color');
-//    var ischecked_method = false;
-//    for ( var i = 0; i < color.length; i++) {
-//        if(color[i].checked) {
-//        ischecked_method = true;
-//        console.log("checked");
-//        break;
-//        }
-//    }
-    
-    
     if (document.getElementById("red").checked) {
-        getmain.style.backgroundColor = red;
-        for(var i=0; i < getschets.length; i++) {
-            getschets[i].style.stroke = offwhite;
+        themeColor = red;
+        strokeColor = offwhite;
+        
+        getMain.style.backgroundColor = themeColor;
+        getBovenkant.style.backgroundColor = themeColor;
+        getOnderkant.style.backgroundColor = themeColor;
+        getRug.style.backgroundColor = themeColor;
+        for(var i=0; i < getSchets.length; i++) {
+            getSchets[i].style.stroke = offwhite;
         }
-        for(var j=0; j < getcijfer.length; j++) {
-            getcijfer[j].style.fill = offwhite;
+        for(var j=0; j < getCijfer.length; j++) {
+            getCijfer[j].style.fill = offwhite;
         }
-        for(var k=0; k < getnotities.length; k++) {
-            getnotities[k].style.stroke = offwhite;
+        for(var k=0; k < getNotities.length; k++) {
+            getNotities[k].style.stroke = offwhite;
         }
     }
     else if (document.getElementById("blue").checked) {
-        getmain.style.backgroundColor = blue;
-        for(var i=0; i < getschets.length; i++) {
-            getschets[i].style.stroke = orange;
+        themeColor = blue;
+        strokeColor = orange;
+        
+        getMain.style.backgroundColor = themeColor;
+        getBovenkant.style.backgroundColor = themeColor;
+        getOnderkant.style.backgroundColor = themeColor;
+        getRug.style.backgroundColor = themeColor;
+        for(var i=0; i < getSchets.length; i++) {
+            getSchets[i].style.stroke = strokeColor;
         }
-        for(var j=0; j < getcijfer.length; j++) {
-            getcijfer[j].style.fill = orange;
+        for(var j=0; j < getCijfer.length; j++) {
+            getCijfer[j].style.fill = strokeColor;
         }
-        for(var k=0; k < getnotities.length; k++) {
-            getnotities[k].style.stroke = orange;
+        for(var k=0; k < getNotities.length; k++) {
+            getNotities[k].style.stroke = strokeColor;
         }
     }
     else if (document.getElementById("orange").checked) {
-        getmain.style.backgroundColor = orange;
-        for(var i=0; i < getschets.length; i++) {
-            getschets[i].style.stroke = offwhite;
+        themeColor = orange;
+        strokeColor = offwhite;
+        
+        getMain.style.backgroundColor = themeColor;
+        getBovenkant.style.backgroundColor = themeColor;
+        getOnderkant.style.backgroundColor = themeColor;
+        getRug.style.backgroundColor = themeColor;
+        for(var i=0; i < getSchets.length; i++) {
+            getSchets[i].style.stroke = strokeColor;
         }
-        for(var j=0; j < getcijfer.length; j++) {
-            getcijfer[j].style.fill = offwhite;
+        for(var j=0; j < getCijfer.length; j++) {
+            getCijfer[j].style.fill = strokeColor;
         }
-        for(var k=0; k < getnotities.length; k++) {
-            getnotities[k].style.stroke = offwhite;
+        for(var k=0; k < getNotities.length; k++) {
+            getNotities[k].style.stroke = strokeColor;
         }
     }
     else if (document.getElementById("blueprint").checked) {
-        getmain.style.backgroundColor = blueprint;
-        for(var i=0; i < getschets.length; i++) {
-            getschets[i].style.stroke = white;
+        themeColor = blueprint;
+        strokeColor = white;
+        
+        getMain.style.backgroundColor = themeColor;
+        getBovenkant.style.backgroundColor = themeColor;
+        getOnderkant.style.backgroundColor = themeColor;
+        getRug.style.backgroundColor = themeColor;
+        for(var i=0; i < getSchets.length; i++) {
+            getSchets[i].style.stroke = strokeColor;
         }
-        for(var j=0; j < getcijfer.length; j++) {
-            getcijfer[j].style.fill = white;
+        for(var j=0; j < getCijfer.length; j++) {
+            getCijfer[j].style.fill = strokeColor;
         }
-        for(var k=0; k < getnotities.length; k++) {
-            getnotities[k].style.stroke = white;
+        for(var k=0; k < getNotities.length; k++) {
+            getNotities[k].style.stroke = strokeColor;
         }
     }
 }
 
+function cardColor() {
+    for(var l=0; l < getCard.length; l++) {
+        if(getCardInput[l].checked == true) {
+            getCardSchets[l].style.backgroundColor = themeColor;
+        } else {
+            getCardSchets[l].style.backgroundColor = "transparent";
+        }
+    }
+}
 
-
-getbutton.addEventListener("click", function() {
+getButton.addEventListener("click", function() {
     perspective();
     checkButton();
 });
 
-gettheme.addEventListener("click", changeColor);
+getTheme.addEventListener("click", function() {
+    changeColor();
+    cardColor();
+});
+
+getGrid.addEventListener("click", cardColor);
